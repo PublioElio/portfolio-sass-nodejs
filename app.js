872cbbd2af92859e2a07bdf0723b20ae28d77d3e
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-07-01 12:58:12 +0200

    hide/show menu

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const toggle = document.querySelector(".nav__wrapper__toggle");
+const toggleIcon = document.querySelector("#navbar_toggle_icon_container");
+const navbarMenuList = document.querySelector("#navbar_menu_list");
 
-// Add click event on toggle icon
-toggle.addEventListener("click", () => {
-  alert("Hello toggle");
+// Add click event on toggle icon and show/hide navbar menu list
+toggleIcon.addEventListener("click", () => {
+    navbarMenuList.classList.toggle('nav__active');
 });
